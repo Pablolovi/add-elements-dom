065f00b8d0eb4cc10fb3cb1c3ecf9da08b91c530
Pablolovi <lvizpablez@gmail.com>
2024-11-07 15:16:43 +0100

    Actualización de estilos y lógica en el ejercicio de agregar elementos a la lista

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 // Aquí tu código
 document.getElementById('agregar').addEventListener('click', function() {
-    // Pedir al usuario que ingrese un elemento
     const elemento = prompt('Introduce un elemento para agregar a la lista:');
   
-    // Verificar si se ingresó un elemento
     if (elemento) {
-      // Crear un nuevo elemento de lista
       const li = document.createElement('li');
-      li.textContent = elemento; // Establecer el texto del elemento
+      li.textContent = elemento;
   
-      // Agregar el nuevo elemento a la lista
       document.getElementById('lista').appendChild(li);
     } else {
       alert('No se ingresó ningún elemento.');
